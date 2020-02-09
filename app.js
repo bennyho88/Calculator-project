688cc0b5e5f0d427e66f74f8c919e1da9f67c6b7
Benny Ho <benny.ho2530@gmail.com>
2020-02-09 18:25:11 +0100

    added function for grey buttons and clear button

--- app.js
+++ app.js
@@ -8,22 +8,88 @@
   const equalButton = document.querySelector('.btn-equal');
   const clearButton = document.querySelector('.btn-clear');
 
-  
+  let input = '';
+ 
   // get values from the yellow buttons
 
   yellowButtons.forEach(yellow => {
     yellow.addEventListener('click', function(e) {
       
+      
       let yellowValue = e.target.getAttribute('data-num')
       console.log(yellowValue)
       
+  
+
+      input += yellowValue;
+
+          // get value to the screen
+      inputScreen.value = input;
+
+      return input;
+      
+      
+      /*
+      if(yellowValue === '*') {
+        input * input
+        console.log('input +: ' * input);
+      } else if (yellowValue === '/') {
+        input / input
+        console.log('input /: ' + input);
+      } else if (yellowValue === '-') {
+        input - input
+        console.log('input -: ' + input);
+      } else if (yellowValue === '+') {
+        input + input
+        console.log('input +: ' + input);
+      }
+      */
     })
   })
 
   // get values from the grey buttons
 
+  greyButtons.forEach(grey => {
+    grey.addEventListener('click', function(e) {
 
+      
+      let greyValue = e.target.getAttribute('data-num');
+      console.log(greyValue)
+      
 
+      // put value to the screen
+      
+    
+      
+      input += greyValue
+      
+      inputScreen.value = input;
+      
+      return input
+     
+      
+    })
+  })
+
+  // get value equal button
+/*
+  equalButton.addEventListener('click', function(e) {
+
+    let total = e.target.value;
+    console.log(total)
+
+    
+  })
+*/
+  // get value reset button
+
+clearButton.addEventListener('click', function() {
+
+  // reset screen
+
+  inputScreen.value = '';
+  input = '';
+})
 
 })();
 
